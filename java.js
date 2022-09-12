be5b1003f1d92d9e1866fe43c1129094b3ccf28d
Elzero Rifay <elzerorifay@gmail.com>
2022-09-12 13:27:09 +0200

    Add new Feature

---
+++ java.js
@@ -0,0 +1,17 @@
+
+
+
+// let todolistarray = []
+// const addButton = document.getElementById("Add");
+// const addInput  = document.getElementById('AddInput').value;
+// addButton.addEventListener("click", function (){
+//     document.getElementById("checkbox-list").innerHTML = addInput;
+//     console.log("object");
+    
+// })
+
+// const deletedButton = document.getElementById("deleted");
+// deletedButton.addEventListener("click",function(){
+//     todolistarray = todolistarray.filter (item=> {
+//         return item.done == false
+//     })
